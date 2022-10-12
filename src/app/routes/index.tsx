43d317a223012d60f8fda333572c2a2162daf810
zac <jadielsonmanoel@gmail.com>
2022-10-11 22:15:31 -0300

    feat: add dashboard page and react router dom

---
+++ src/app/routes/index.tsx
@@ -0,0 +1,18 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes as Switch,
+  Navigate
+} from "react-router-dom";
+import { DashBoard } from "../pages";
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/pagina-inicial" element={<DashBoard />} />
+        <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
